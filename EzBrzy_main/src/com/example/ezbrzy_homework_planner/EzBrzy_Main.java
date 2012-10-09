807f3302208b83fe0ce3bb1c7ef94a3f1e1351aa
Eric Wilcox <ewilcox42@gmail.com>
2012-10-09 15:59:48 -0600

    Added files for PhoneGap and dependencies
    Included assets index.html, css & js folders.  Added all permissions and
    extended DroidGap in the java src file.

--- EzBrzy_main/src/com/example/ezbrzy_homework_planner/EzBrzy_Main.java
+++ EzBrzy_main/src/com/example/ezbrzy_homework_planner/EzBrzy_Main.java
@@ -3,13 +3,14 @@ package com.example.ezbrzy_homework_planner;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import org.apache.cordova.*;
 
-public class EzBrzy_Main extends Activity {
+public class EzBrzy_Main extends DroidGap {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ez_brzy__main);
+        super.loadUrl("file:///android_asset/www/index.html");
     }
 
     @Override
