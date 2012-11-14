36a9436f9e304a1e0c10b344a2557a307f32d3ad
Eric Wilcox <ewilcox42@gmail.com>
2012-11-14 13:54:22 -0700

    Pulled all local file storage functions out, removed notes page delete all button, moved onFSSuccess code to onDeviceReady
    also added junk file for code snippits we want to have quick reference
    too

--- EzBrzy_main/assets/www/js/app.js
+++ EzBrzy_main/assets/www/js/app.js
@@ -1,5 +1,4 @@
-var fileSystem,
-	dbShell;
+var dbShell;
 
 //generic error handler
 function onError(e) {
@@ -11,77 +10,9 @@ function dbErrorHandler(err) {
 	alert("DB Error : " + err.message + "\n\nCode=" + err.code);
 }
 
-function logNote(data) {
-	//var output = '<li>' + data + '</li>';
-    $('#noteContent ul').html(data).listview('refresh');
-	//this works too-->//getById("#noteContent ul").innerHTML = data;
-}
-
-function doLog(data) {
-	//console.log(data);
-}
-
 //generic getById
 function getById(id) {
     return document.querySelector(id);
-}
-
-function readNote(f) {
-    reader = new FileReader();
-    reader.onloadend = function(e) {
-        logNote("<pre>" + e.target.result + "</pre>");
-    };
-    reader.readAsText(f);
-}
-
-function doReadNotes() {
-	fileSystem.root.getFile("notes.txt", {create:true}, readNote, onError);
-}
-
-function saveNote(f) {
-	var ref = '#';
-	f.createWriter(function(writerOb) {
-		writerOb.onwrite = function() {
-			doReadNotes();
-		};
-		//go to the end of the file...
-		writerOb.seek(writerOb.length);
-		writerOb.write('<li><a href="' + ref + '">Test at ' + new Date().toString() + '</a></li>');
-	});
-}
-
-function doSaveNote() {
-	fileSystem.root.getFile("notes.txt", {create:true}, saveNote, onError);
-}
-
-function doDeleteNotes(e) {
-    fileSystem.root.getFile("notes.txt", {create:true}, function(f) {
-        f.remove(function() {
-            doReadNotes();
-            alert("Notes file deleted");
-        });
-    }, onError);
-}
-
-function renderEntries(tx,results) {
-	//alert("render entries reached");
-	var $content = $('#assignmentContent'),
-		id,
-		title;
-	if (results.rows.length === 0) { $content.html('<p>You do not have any notes currently</p>'); }
-	else {
-		var s = "";
-		for (var i=0; i<results.rows.length; i++) {
-			id = results.rows.item(i).id;
-			title = results.rows.item(i).title;
-			s += '<li><a href="#addAssignment" data-role="button" data-transition="none" data-direction="reverse">' + id + " - " + title + '</a></li>';
-//			s += "<li><a href='edit.html?id="+results.rows.item(i).id + "'>" + results.rows.item(i).title + "</a></li>"; //pick up here!!
-		}
-		id = null;
-		title = null;
-		$content.html(s)
-			.listview('refresh');
-	}
 }
 
 function getEntries() {
@@ -114,147 +45,33 @@ function displayListing() {
 	$('#assignmentsDisplay').html(outputAssign);
 	$('#coursesDisplay').html(outputCourses);
 	$('#notesDisplay').html(outputNotes);
-	doReadNotes();
+	//doReadNotes();	//TODO: should go away
 }
 
-function onFSSuccess(fs) {
-    fileSystem = fs;
+function setupDB() {
+	dbShell = window.openDatabase("ezbrzy","1.0","EzBrzy Database",1000000);
+	dbShell.transaction(setupTable,dbErrorHandler,getEntries);
+}
+
+function onDeviceReady() {
+	setupDB();
 	displayListing();
-	//writeData();
-	getById("#saveNote").addEventListener("touchstart",doSaveNote);
-	getById("#deleteNotes").addEventListener("touchstart",doDeleteNotes);
-	getById('#saveAssignment').addEventListener("touchstart",saveAssignment);
-	
-    dbShell = window.openDatabase("ezbrzy","1.0","EzBrzy Database",1000000);
-    dbShell.transaction(setupTable,dbErrorHandler,getEntries);
-    
+//	getById("#saveNote").addEventListener("touchstart",doSaveNote);
+//	getById("#deleteNotes").addEventListener("touchstart",doDeleteNotes);
+//	getById('#saveAssignment').addEventListener("touchstart",saveAssignment);
     //handle form submission of a new/old note
-    $("#editNoteForm").live("submit",function(e) {
+    
+	$("#editNoteForm").live("submit",function(e) {
         var data = {title:$("#assignDesc").val(), 
                     body:$("#assignDateDue").val(),
                     id:$("#noteId").val()
         };
     saveAssignment(data,function() {
         $.mobile.changePage("#assignments",{reverse:true});
+        });
     });
-    });
-    
-/*	
-    getById("#dirListingButton").addEventListener("touchstart",doDirectoryListing);            
-    getById("#addFileButton").addEventListener("touchstart",doAppendFile);            
-    getById("#readFileButton").addEventListener("touchstart",doReadFile);            
-    getById("#metadataFileButton").addEventListener("touchstart",doMetadataFile);            
-    getById("#deleteFileButton").addEventListener("touchstart",doDeleteFile);            
-    getById("#addAlertData").addEventListener("touchstart",doAlertData);
-    
-    logit( "Got the file system: "+fileSystem.name +" " +
-                                    "root entry name is "+fileSystem.root.name + "<p/>")    
-    doDirectoryListing();
-*/
-}
-
-function onDeviceReady() {
-    //request the persistent file system
-    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onFSSuccess, onError);
 }
 
 function init() {
     document.addEventListener("deviceready", onDeviceReady, true);
-}
-
-//generic content logger
-function logit(s) {
-    getById("#content").innerHTML = s;
-}
-
-function doDeleteFile(e) {
-    fileSystem.root.getFile("test.txt", {create:true}, function(f) {
-        f.remove(function() {
-            logit("File removed<p/>");
-        });
-    }, onError);
-}
-
-function metadataFile(m) {
-    logit("File was last modified "+m.modificationTime+"<p/>");    
-}
-
-function doMetadataFile(e) {
-    fileSystem.root.getFile("test.txt", {create:true}, function(f) {
-        f.getMetadata(metadataFile,onError);
-    }, onError);
-}
-
-function readFile(f) {
-    reader = new FileReader();
-    reader.onloadend = function(e) {
-        console.log("go to end");
-        logit("<pre>" + e.target.result + "</pre><p/>");
-    };
-    reader.readAsText(f);
-}
-
-function doReadFile(e) {
-    fileSystem.root.getFile("test.txt", {create:true}, readFile, onError);
-}
-
-function appendFile(f) {
-    f.createWriter(function(writerOb) {
-        writerOb.onwrite=function() {
-            logit("Done writing to file.<p/>");
-        };
-        //go to the end of the file...
-        writerOb.seek(writerOb.length);
-        writerOb.write("Test at "+new Date().toString() + "\n");
-    });
-}
-
-function doAppendFile(e) {
-    fileSystem.root.getFile("test.txt", {create:true}, appendFile, onError);
-}
-
-function gotFiles(entries) {
-    var s = "";
-    for(var i=0,len=entries.length; i<len; i++) {
-        //entry objects include: isFile, isDirectory, name, fullPath
-        s+= entries[i].fullPath;
-        if (entries[i].isFile) {
-            s += " [F]";
-        }
-        else {
-            s += " [D]";
-        }
-        s += " ";
-        
-    }
-    s+="<p/>";
-    logit(s);
-}
-
-function doDirectoryListing(e) {
-    //get a directory reader from our FS
-    var dirReader = fileSystem.root.createReader();
-
-    dirReader.readEntries(gotFiles,onError);        
-}
-
-function alertData(f){
-	f.createWriter(function(writerOb) {
-       writerOb.onwrite=function() {
-            alert("Wrote to the file!");
-       };
-        //go to the end of the file...
-       writerOb.seek(writerOb.length);
-       writerOb.write("I wrote some new test data!\n");
-   });
-}
-
-function doAlertData(e) {
-		fileSystem.root.getFile("test.txt", {create:true}, alertData, onError);
-}
-
-function writeData() {
-	var output="";
-	for (i=0; i<20; i++) { output += "<p>test data ... "+ i + "</p>"; }
-	$("#testData").html(output);
 }
